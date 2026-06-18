c1be2668d15a96a96a0a8b6728a6667f236f7d9d
Gia Huy Duong <n24dtcn038@student.ptithcm.edu.vn>
2026-06-18 08:22:31 +0700

    hw

--- src/main/java/com/example/hrm/Bai4Application.java
+++ src/main/java/com/example/hrm/Bai4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Bai3Application {
+public class Bai4Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Bai3Application.class, args);
+        SpringApplication.run(Bai4Application.class, args);
     }
 
 }
--- src/main/java/com/example/hrm/security/UserDetailServiceCustom.java
+++ src/main/java/com/example/hrm/security/UserDetailServiceCustom.java
@@ -11,11 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
-// Đánh dấu đây là một Service để Spring Boot tự động quản lý (IoC Container)
 @Service
 public class UserDetailServiceCustom implements UserDetailsService {
 
-    // Tiêm (Inject) UserRepository vào để tương tác với Database
     private final UserRepository userRepository;
 
     @Autowired
@@ -23,21 +21,14 @@ public class UserDetailServiceCustom implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-    // Ghi đè phương thức cốt lõi của Spring Security
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // 1. Tìm kiếm người dùng trong Database
-        // Sử dụng orElseThrow của Optional để ném ra ngoại lệ ngay lập tức nếu kết quả là null
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Không tìm thấy tài khoản: " + username));
 
-        // 2. Chuyển đổi quyền hạn
-        // Nối thêm tiền tố "ROLE_" để tương thích hoàn toàn với các hàm phân quyền như hasRole()
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
 
-        // 3. Đóng gói Entity User và quyền hạn vào UserPrincipal để trả về cho Spring Security
-        // Vì ở đây mỗi user chỉ có 1 role, ta dùng Collections.singletonList để tạo List chứa 1 phần tử
         return new UserPrincipal(user, Collections.singletonList(authority));
     }
 }
